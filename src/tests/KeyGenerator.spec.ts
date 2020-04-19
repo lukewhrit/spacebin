b1aa3177fceea057f63f2430370912d747293cf2
324Luke <me@lukewhrit.xyz>
2020-04-19 19:37:07 -0400

    test(key-generator): remove test to check if createKey() alternates consonants and vowels
    This test was causing the build to error on occasion and was generally pointless

--- src/tests/KeyGenerator.spec.ts
+++ src/tests/KeyGenerator.spec.ts
@@ -6,15 +6,3 @@ test('createKey() returns key of proper length ', t => {
 
   t.is(g.createKey(6).length, 6)
 })
-
-const vowels = 'aeiou'
-const consonants = 'bcdfghjklmnpqrstvwxyz'
-
-test('createKey() alternates consonants and vowels', t => {
-  const g = new PhoneticKeyGenerator()
-  const k = g.createKey(3)
-
-  t.true(consonants.includes(k[0]))
-  t.true(consonants.includes(k[2]))
-  t.true(vowels.includes(k[1]))
-})
