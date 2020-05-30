8f12653f1664f34dfd96fc2099e6d03693edc4e5
Luke Whrit <lukewhrit@gmail.com>
2020-05-30 09:17:15 -0400

    refactor(keygenerator): move randOf function to class

--- src/keyGenerator.ts
+++ src/keyGenerator.ts
@@ -1,15 +1,15 @@
 // Draws inspiration from pwgen and http://tools.arantius.com/password
 
-const randOf = (collection: string | string[]): Function => {
-  return (): string => {
-    return collection[Math.floor(Math.random() * collection.length)]
-  }
-}
-
 export class PhoneticKeyGenerator {
+  private randOf = (collection: string | string[]): Function => {
+    return (): string => {
+      return collection[Math.floor(Math.random() * collection.length)]
+    }
+  }
+
   // Helper methods to get an random vowel or consonant
-  private randVowel = randOf('aeiou')
-  private randConsonant = randOf('bcdfghjklmnpqrstvwxyz')
+  private randVowel = this.randOf('aeiou')
+  private randConsonant = this.randOf('bcdfghjklmnpqrstvwxyz')
 
   // Generate a phonetic key of alternating consonant & vowel
   createKey (keyLength: number): string {
