c1d951b43ada9d5466c01cdf551ffe6b2952c72b
Luke Whrit <lukewhrit@gmail.com>
2020-06-03 15:03:04 -0400

    feat: get document route

--- src/routes/document.route.ts
+++ src/routes/document.route.ts
@@ -1,11 +1,27 @@
 import Router from '@koa/router'
+import { createConnection } from 'typeorm'
+import { Document } from '../entities/document.entity'
+import { dbOptions } from '../controllers/database.controller'
 
-export default (router: Router): void => {
+export default async (router: Router): Promise<void> => {
+  const connection = await createConnection(dbOptions)
+  const documents = connection.getRepository(Document)
+
   router.post('create document', '/document', async (ctx) => {
     ctx.status = 501
   })
 
   router.get('get document', '/document', async (ctx) => {
-    ctx.status = 501
+    /*
+      Body: {
+        "key": "xqm5wXXE"
+      }
+     */
+
+    const doc = await documents.findOne({
+      where: { id: ctx.request.body.key }
+    })
+
+    ctx.body = doc
   })
 }
