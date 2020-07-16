b6372fc2747c63c3044eeaef717f049fd92307b9
Luke Whrit <lukewhrit@gmail.com>
2020-07-15 23:39:54 -0400

    chore: fix gitignore and styling

--- cmd/hello.go
+++ cmd/hello.go
@@ -1,5 +1,5 @@
 package hello
 
 func Hello() string {
-    return "Hello, world."
+	return "Hello, world."
 }
--- cmd/hello_test.go
+++ cmd/hello_test.go
@@ -3,8 +3,9 @@ package hello
 import "testing"
 
 func TestHello(t *testing.T) {
-    want := "Hello, world."
-    if got := Hello(); got != want {
-        t.Errorf("Hello() = %q, want %q", got, want)
-    }
+	want := "Hello, world."
+
+	if got := Hello(); got != want {
+		t.Errorf("Hello() = %q, want %q", got, want)
+	}
 }
