aa425e07e2eefe8bb5dca318d63818f3577d3262
Luke Whrit <lukewhrit@gmail.com>
2020-07-17 18:54:26 -0400

    test(document): add more tests

--- src/__tests__/document.route.spec.ts
+++ src/__tests__/document.route.spec.ts
@@ -32,4 +32,32 @@ describe('Document Endpoints', () => {
 
     expect(res.status).toBe(400)
   })
+
+  it('should succeed on create document request', async () => {
+    const res = await request(app)
+      .post(`${baseURL}/`)
+      .send({
+        content: 'this is a test'
+      })
+
+    expect(res.body).toHaveProperty('payload.id')
+    expect(res.body).toHaveProperty('payload.contentHash', '2e99758548972a8e8822ad47fa1017ff72f06f3ff6a016851f45c398732bc50c')
+  })
+
+  it('should properly return document object')
+
+  it('should return raw content of document')
+
+  it('should send status code 200 if document exists', async () => {
+    // im unsure how to properly implement this...
+    // it would require having a document we're sure exists in the database otherwise it will give a false negative.
+  })
+
+  it('should send status code 404 if document does not exist', async () => {
+    const res = await request(app)
+      .get(`${baseURL}/abcdefgh/verify`)
+
+    expect(res.status).toBe(404)
+    expect(res.body).toHaveProperty('payload.exists', false)
+  })
 })
