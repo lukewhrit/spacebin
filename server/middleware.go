d0f74761f9099f34614edbada934e828d0a25d6d
Luke Whrit <lukewhrit@gmail.com>
2020-08-13 14:49:33 -0400

    refactor(server): move security middleware into registerMiddleware() function

--- server/middleware.go
+++ server/middleware.go
@@ -20,6 +20,25 @@ func registerMiddlewares(app *fiber.App) {
 	}))
 
 	app.Use(cors.New())
-	app.Use(SecurityHeaders())
 	app.Use(middleware.Logger())
+
+	// Custom middleware to set security-related headers
+	app.Use(func(c *fiber.Ctx) {
+		// Set some security headers:
+		c.Set("X-Download-Options", "noopen")
+		c.Set("X-DNS-Prefetch-Control", "off")
+		c.Set("X-Frame-Options", "SAMEORIGIN")
+		c.Set("X-XSS-Protection", "1; mode=block")
+		c.Set("X-Content-Type-Options", "nosniff")
+		c.Set("Referrer-Policy", "no-referrer-when-downgrade")
+		c.Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
+		c.Set("Cache-Control", "max-age=31536000")
+
+		if config.Config.Server.UseCSP == true {
+			c.Set("Content-Security-Policy", "default-src 'none'; frame-ancestors 'none'; base-uri 'none'; form-action 'none';")
+		}
+
+		// Go to next middleware:
+		c.Next()
+	})
 }
