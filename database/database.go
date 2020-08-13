66e219b8df9b6eac12304cd69a4b8bdf731e054d
Luke Whrit <lukewhrit@gmail.com>
2020-08-12 22:22:47 -0400

    refactor(database): move Init() function to database.go

--- database/database.go
+++ database/database.go
@@ -1,8 +1,21 @@
 package database
 
-import "github.com/gobuffalo/pop"
+import (
+	"log"
 
-var (
-	// DBConn holds the current connection to the database
-	DBConn *pop.Connection
+	"github.com/gobuffalo/pop"
 )
+
+// DBConn holds the current connection to the database
+var DBConn *pop.Connection
+
+// Init opens a connection to the database
+func Init() {
+	var err error
+
+	DBConn, err = pop.Connect("main")
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %e", err)
+	}
+}
