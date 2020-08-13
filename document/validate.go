3a780ed4191299559f22da31850b14fe5cd2c1b6
Luke Whrit <lukewhrit@gmail.com>
2020-08-12 23:38:39 -0400

    feat(validators): update "extension" field rules

--- document/validate.go
+++ document/validate.go
@@ -15,18 +15,32 @@ type CreateRequest struct {
 
 // Validate performs validation on the body
 func (c CreateRequest) Validate() error {
-	regex := regexp.MustCompile("^txt$|^js$")
+	/*
+	 * This regex matches the file extension for various languages.
+
+	 * Languages including:
+	 * 	Python, JavaScript(React), TypeScript(React), Go, Kotlin,
+	 *	C++, SQL, C-Sharp, C (including `.h`), Scala, Haskell,
+	 * 	Shell, PowerShell, PHP, Assembly, Julia, Objective-C (.m),
+	 *	Perl, Crystal, JSON, YAML, TOML, and Plain Text
+
+	 * For any unsupported formats Plain Text should be used.
+	 */
+	regex := regexp.MustCompile("^py$|^[tj](sx|s)$|^go$|^kt$|^java$|^c$|^c(pp|[rs])$|^sql$|^swift$|^dart$|^r$|^r[sb]$|^h$|^scala$|^hs$|^sh$|^p(s1|hp)$|^asm$|^jl$|^m$|^pl$|^(x|ya|to)ml$")
 
 	return validation.ValidateStruct(&c,
 		validation.Field(
 			&c.Content,
 			validation.Required,
-			// Enforce length to follow
+			// Enforce length to follow what's set in the config
 			validation.Length(2, config.Config.Documents.MaxDocumentLength),
 		),
+		// The purpose of this field is to support client's that perform
+		// syntax highlighting and need to know what highlighter to use.
 		validation.Field(
 			&c.Extension,
 			validation.Match(regex),
+			validation.Required,
 		),
 	)
 }
