9ec59d4dd5763fc9aa6c65ae90b4fb47ed2b019a
Luke Whrit <lukewhrit@gmail.com>
2020-09-01 20:07:50 -0400

    build(mage): add ability to disable sqlite building

--- magefile.go
+++ magefile.go
@@ -2,15 +2,27 @@
 
 package main
 
-import "github.com/magefile/mage/sh"
+import (
+	"os"
+
+	"github.com/magefile/mage/sh"
+)
 
 // Build generates a binary of the project
 func Build() error {
-	if err := sh.Run("go", "mod", "download"); err != nil {
-		return err
-	}
+	if os.Getenv("NO_SQLITE") == "1" {
+		if err := sh.Run("go", "mod", "download"); err != nil {
+			return err
+		}
 
-	return sh.Run("go", "build", "--ldflags", "-s -w", "-tags", "sqlite", "-o", "bin/curiosity", "./")
+		return sh.Run("go", "build", "--ldflags", "-s -w", "-o", "bin/curiosity", "./")
+	} else {
+		if err := sh.Run("go", "mod", "download"); err != nil {
+			return err
+		}
+
+		return sh.Run("go", "build", "--ldflags", "-s -w", "-tags", "sqlite", "-o", "bin/curiosity", "./")
+	}
 }
 
 // Format lints and fixes all files in the directory
