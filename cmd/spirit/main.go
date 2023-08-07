ff962fe9ed37dda3add64a4826b053e442b2ff42
Luke <lukewhrit@pm.me>
2023-08-07 19:53:57 -0400

    docs: update copyright

--- cmd/spirit/main.go
+++ cmd/spirit/main.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2023 Luke Whrit, Jack Dorland
+ * Copyright 2020-2023 Luke Whritenour
 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
