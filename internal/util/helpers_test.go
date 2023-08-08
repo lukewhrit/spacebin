1787bdda4b78d38bd7916d5e03584d0a8710bce7
Luke <lukewhrit@pm.me>
2023-08-08 14:03:26 -0400

    test(util/helpers_test.go): add tests for WriteJSON and WriteError

--- internal/util/helpers_test.go
+++ internal/util/helpers_test.go
@@ -17,7 +17,12 @@
 package util_test
 
 import (
+	"encoding/json"
+	"errors"
 	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/orca-group/spirit/internal/util"
@@ -44,4 +49,59 @@ func TestCountLines(t *testing.T) {
 	lines := util.CountLines(content)
 
 	require.Equal(t, lines, template.HTML("<div>1</div><div>2</div>"))
+}
+
+func TestHandleBodyDetection(t *testing.T) {}
+func TestHandleBodyJSON(t *testing.T)      {}
+func TestHandleBodyMultipart(t *testing.T) {}
+
+func TestWriteJSON(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := util.WriteJSON[map[string]interface{}](w, 200, map[string]interface{}{
+			"test": "test",
+		})
+
+		require.NoError(t, err)
+	}))
+	defer server.Close()
+
+	res, err := http.Get(server.URL)
+	require.NoError(t, err)
+
+	require.Equal(t, http.StatusOK, res.StatusCode)
+
+	x, _ := io.ReadAll(res.Body)
+	var body map[string]interface{}
+	json.Unmarshal(x, &body)
+
+	require.Equal(t, body, map[string]interface{}{
+		"payload": map[string]interface{}{
+			"test": "test",
+		},
+		"error": "",
+	})
+}
+
+func TestWriteError(t *testing.T) {
+	e := errors.New("some error")
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := util.WriteError(w, http.StatusInternalServerError, e)
+		require.NoError(t, err)
+	}))
+	defer server.Close()
+
+	res, err := http.Get(server.URL)
+	require.NoError(t, err)
+
+	require.Equal(t, http.StatusInternalServerError, res.StatusCode)
+
+	x, _ := io.ReadAll(res.Body)
+	var body map[string]interface{}
+	json.Unmarshal(x, &body)
+
+	require.Equal(t, body, map[string]interface{}{
+		"payload": map[string]interface{}{},
+		"error":   e.Error(),
+	})
 }
